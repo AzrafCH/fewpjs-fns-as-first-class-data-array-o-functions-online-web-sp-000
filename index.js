6b68f28f577bc47949448ee2ea4df2aa8b5c4603
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-12 18:56:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function unleashDog(dogName, dogBreed) {
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
 
 function exerciseDog(dogName, dogBreed) {
-  return routine.map(fn => fn(dog, breed))
+  return routine.map(fn => fn(dogName,dogBreed))
 }
